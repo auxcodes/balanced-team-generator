ea2de9d98b5829c6bfa3a66f7fa1be1cda682ae1
Vicky Patel <vicky@Vickys-MBP.attlocal.net>
2024-04-15 23:44:43 -0400

    Added new mocks for testing

---
+++ src/mocks/PlayerMock.tsx
@@ -0,0 +1,27 @@
+import { PlayerModel } from "../components/Pages/CreateTeamsWorflow/Models/CreateTeamsModels";
+
+export const PlayerList: PlayerModel[] = [
+    { name: 'Vicky', rating: 1 },
+    { name: 'Bappi', rating: 2 },
+    { name: 'HJ', rating: 4.5 },
+    { name: 'Allu', rating: 5.0 },
+    { name: 'Kent', rating: 4.0 },
+    { name: 'Neal', rating: 1 },
+    { name: 'Nishil', rating: 2 },
+    { name: 'JK', rating: 4.5 },
+    { name: 'DD', rating: 5.0 },
+    { name: 'PP', rating: 4.0 },
+    { name: 'Pintu', rating: 1 },
+    { name: 'JP', rating: 2.5 },
+    { name: 'Jiya', rating: 3.0 },
+    { name: 'Mamta', rating: 3.0 },
+    { name: 'Roshanda', rating: 3.5 },
+    { name: 'Nikki', rating: 3.0 },
+    { name: 'Sesha', rating: 4.0 },
+    { name: 'Bhartiya', rating: 3.5 },
+    { name: 'Krups', rating: 4.5 },
+    { name: 'Sanjay Dutt', rating: 4.0 },
+    { name: 'Roops', rating: 3.5 }
+];
+
+export const DynamicPlayerList: PlayerModel[] = []
